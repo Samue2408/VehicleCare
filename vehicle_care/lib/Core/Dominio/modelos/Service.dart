853431355a11f35b10c3ab0e6038887298c13287
Elián Gutierrez Escorcia <eliandgutierreze@outlook.com>
2024-05-30 17:11:03 -0500

    fromsjsom users

--- vehicle_care/lib/Core/Dominio/modelos/Service.dart
+++ vehicle_care/lib/Core/Dominio/modelos/Service.dart
@@ -10,4 +10,13 @@ class Service {
     required this.name,
     required this.intervale
   });
+
+
+  factory Service.fromJson(Map<String, dynamic> json) {
+    return Service(
+      id: json['id'], 
+      name: json['name'], 
+      intervale: json['intervale'],
+      );
+  }
 }
--- vehicle_care/lib/Core/Dominio/modelos/User.dart
+++ vehicle_care/lib/Core/Dominio/modelos/User.dart
@@ -5,7 +5,7 @@ class Users {
   final int id;
   final String name;
   final String email;
-  final String cellphone;
+  final int cellphone;
   final String password;
   final List<Cars> cars;
 
@@ -19,4 +19,19 @@ class Users {
       required this.cars,
     }
   );
+
+  factory Users.fromJson(Map<String, dynamic> json) {
+
+    var var_carslist = json['cars'] as List;
+    List<Cars> cars_list = var_carslist.map((item) => Cars.fromJson(item)).toList();
+
+    return Users(
+      id: json['id'], 
+      name: json['name'], 
+      email: json['email'],
+      cellphone: json['cellphone'],
+      password: json['password'],
+      cars: cars_list,
+      );
+  }
 }
